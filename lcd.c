3c865d7e1fef11aa425354153e0ec28a5729764f
Akshay Gajare <71394698+DeveloperAkshayG@users.noreply.github.com>
2021-01-24 18:00:15 +0530

    lcd source codes with header file
    lcd source code with header file

---
+++ lcd.c
@@ -0,0 +1,71 @@
+#define F_CPU 8000000UL   //CPU FREQUENCY 8MHZ
+#include "lcd.h"		  //LCD header file
+
+void LCD_Command(unsigned char cmnd)
+{
+	LCD_Data_Port= cmnd;
+	LCD_Command_Port &= ~(1<<RS);	/* RS=0 command reg. */
+	_delay_ms(1);
+	LCD_Command_Port &= ~(1<<RW);	/* RW=0 Write operation */
+	_delay_ms(1);
+	LCD_Command_Port |= (1<<EN);	/* Enable pulse */
+	_delay_ms(1);
+	LCD_Command_Port &= ~(1<<EN);
+	_delay_ms(1);
+}
+
+void LCD_Char (unsigned char char_data)	/* LCD data write function */
+{
+	LCD_Data_Port= char_data;
+	LCD_Command_Port |= (1<<RS);	/* RS=1 Data reg. */
+	_delay_ms(1);
+	LCD_Command_Port &= ~(1<<RW);	/* RW=0 write operation */
+	_delay_ms(1);
+	LCD_Command_Port |= (1<<EN);	/* Enable Pulse */
+	_delay_ms(1);
+	LCD_Command_Port &= ~(1<<EN);
+	_delay_ms(1);
+}
+
+void LCD_Init(void)
+{
+	LCD_Command_Dir = 0xFF;
+	LCD_Data_Dir = 0xFF;		
+	_delay_ms(20);			
+	
+	LCD_Command (0x38);		//initialize in 8-bit mode
+	_delay_ms(1);
+	LCD_Command (0x0C);		//display on cursor off
+	_delay_ms(1);
+	LCD_Command (0x06);		//autoincrement cursor
+	_delay_ms(1);
+	LCD_Command (0x01);		//clear screen
+	_delay_ms(1);
+	LCD_Command (0x80);     //cursor at starting point
+}
+
+void LCD_Clear(void)
+{
+	LCD_Command (0x01);		/* clear display */
+	_delay_ms(1);
+	LCD_Command (0x80);		/* cursor at home position */
+	_delay_ms(1);
+}
+
+void LCD_String (char *str)		/* Send string to LCD function */
+{
+	int i;
+	for(i=0;str[i]!=0;i++)		/* Send each char of string till the NULL */
+	{
+		LCD_Char (str[i]);
+	}
+}
+
+void LCD_String_xy (char row, char pos, char *str)/* Send string to LCD with xy position */
+{
+	if (row == 0 && pos<16)
+	LCD_Command((pos & 0x0F)|0x80);	/* Command of first row and required position<16 */
+	else if (row == 1 && pos<16)
+	LCD_Command((pos & 0x0F)|0xC0);	/* Command of first row and required position<16 */
+	LCD_String(str);		/* Call LCD string function */
+}
